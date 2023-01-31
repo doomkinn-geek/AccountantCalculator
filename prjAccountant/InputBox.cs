31b457ac23d1ce0a28726bef924d8f6f9f731146
root <root@DESKTOP-T51K5CR>
2023-01-31 16:50:42 +0300

    Добавьте файлы проекта.

---
+++ prjAccountant/InputBox.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Accountant
+{
+    public class InputBox
+    {
+
+        Window Box = new Window();//window for the inputbox
+        FontFamily font = new FontFamily("Tahoma");//font for the whole inputbox
+        int FontSize = 30;//fontsize for the input
+        StackPanel sp1 = new StackPanel();// items container
+        string title = "InputBox";//title as heading
+        string boxcontent;//title
+        string defaulttext = "Enter the text...";//default textbox content
+        string errormessage = "Text is not correct";//error messagebox content
+        string errortitle = "Error";//error messagebox heading title
+        string okbuttontext = "OK";//Ok button content
+        string cancelbuttontext = "Cancel";//Cancel button content
+        Brush BoxBackgroundColor = Brushes.GreenYellow;// Window Background
+        Brush InputBackgroundColor = Brushes.Ivory;// Textbox Background
+        bool clicked = false;
+        TextBox input = new TextBox();
+        Button ok = new Button();
+        Button cancel = new Button();
+        bool inputreset = false;
+        public InputBox(string content)
+        {
+            try
+            {
+                boxcontent = content;
+            }
+            catch { boxcontent = "Error!"; }
+            windowdef();
+        }
+        public InputBox(string content, string Htitle, string DefaultText)
+        {
+            try
+            {
+                boxcontent = content;
+            }
+            catch { boxcontent = "Error!"; }
+            try
+            {
+                title = Htitle;
+            }
+            catch
+            {
+                title = "Error!";
+            }
+            try
+            {
+                defaulttext = DefaultText;
+            }
+            catch
+            {
+                DefaultText = "Error!";
+            }
+            windowdef();
+        }
+        public InputBox(string content, string Htitle, string Font, int Fontsize)
+        {
+            try
+            {
+                boxcontent = content;
+            }
+            catch { boxcontent = "Error!"; }
+            try
+            {
+                font = new FontFamily(Font);
+            }
+            catch { font = new FontFamily("Tahoma"); }
+            try
+            {
+                title = Htitle;
+            }
+            catch
+            {
+                title = "Error!";
+            }
+            if (Fontsize >= 1)
+                FontSize = Fontsize;
+            windowdef();
+        }
+        private void windowdef()// window building - check only for window size
+        {
+            Box.WindowStyle = WindowStyle.None;
+            Box.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            Box.Height = 150;// Box Height
+            Box.Width = 300;// Box Width
+            //Box.Background = BoxBackgroundColor;
+            Box.Title = title;
+            Box.Content = sp1;
+            Box.Closing += Box_Closing;
+            Box.MouseLeftButtonDown += Box_MouseLeftButtonDown;
+            TextBlock content = new TextBlock();
+            content.Margin = new Thickness(10, 10, 10, 10);
+            content.TextWrapping = TextWrapping.Wrap;
+            content.Background = null;
+            content.HorizontalAlignment = HorizontalAlignment.Left;
+            content.Text = boxcontent;
+            //content.FontFamily = font;
+            //content.FontSize = FontSize;
+            sp1.Children.Add(content);
+
+            //input.Background = InputBackgroundColor;
+            //input.FontFamily = font;
+            //input.FontSize = FontSize;
+            input.HorizontalAlignment = HorizontalAlignment.Left;
+            input.Margin = new Thickness(10, 10, 10, 10);
+            input.Text = defaulttext;
+            input.MinWidth = 265;
+            input.MouseEnter += input_MouseDown;
+            sp1.Children.Add(input);
+            ok.Width = 70;
+            ok.Height = 20;
+            ok.Click += ok_Click;
+            ok.Content = okbuttontext;
+            ok.HorizontalAlignment = HorizontalAlignment.Left;
+            ok.Margin = new Thickness(10, 0, 10, 10);
+
+            cancel.Width = 70;
+            cancel.Height = 20;
+            cancel.Click += cancel_Click;
+            cancel.Content = cancelbuttontext;
+            cancel.HorizontalAlignment = HorizontalAlignment.Left;
+            cancel.Margin = new Thickness(10, 0, 10, 10);
+
+            sp1.Children.Add(ok);
+            sp1.Children.Add(cancel);
+
+        }
+        void Box_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (!clicked)
+                e.Cancel = true;
+        }
+        private void input_MouseDown(object sender, MouseEventArgs e)
+        {
+            if ((sender as TextBox).Text == defaulttext && inputreset == false)
+            {
+                (sender as TextBox).Text = null;
+                inputreset = true;
+            }
+        }
+        void ok_Click(object sender, RoutedEventArgs e)
+        {
+            clicked = true;
+            if (input.Text == defaulttext || input.Text == "")
+                MessageBox.Show(errormessage, errortitle);
+            else
+            {
+                Box.Close();
+            }
+            clicked = false;
+        }
+
+        void cancel_Click(object sender, RoutedEventArgs e)
+        {
+            clicked = true;
+            Box.Close();            
+        }
+
+        void Box_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Box.DragMove();
+        }
+
+        public string ShowDialog()
+        {
+            Box.ShowDialog();
+            return input.Text;
+        }
+    }
+}
---
+++ prjAccountant/MainWindow.xaml.cs
@@ -0,0 +1,157 @@
+﻿using Accountant;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace prjAccountant
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            string[] strs = sourceText.Text.Split('\n');
+            Dictionary<string, int> counts = new Dictionary<string, int>();
+            string key;
+            int num, spaceIndex;
+            bool noNameValue = false;//значение, как на рынке: много сумм, а в конце название
+            int noNameRes = 0;
+
+            for (int i = 0; i < strs.Length; i++)
+            {
+                if (strs[i].Trim() != "")
+                {                  
+                    spaceIndex = strs[i].IndexOf(" ");
+                    if (spaceIndex == -1)
+                    {
+                        noNameValue = true;
+                        try
+                        {
+                            num = Convert.ToInt16(strs[i]);
+                        }
+                        catch (Exception)
+                        {
+                            num = 0;
+                        }
+                        noNameRes += num;
+                        continue;
+                    }
+                    num = Convert.ToInt16(strs[i].Substring(0, spaceIndex));
+                    key = strs[i].Substring(spaceIndex, (strs[i].Length - (spaceIndex + 1)));
+                    key = key.Trim();
+                    key = key.ToLower();
+
+                    if (num < 400)
+                    {
+                    }
+                    if (!counts.ContainsKey(key))
+                    {
+                        counts.Add(key, 0);
+                        counts[key] = num;
+                        if (noNameValue)
+                        {
+                            counts[key] += noNameRes;
+                            noNameValue = false;
+                        }
+                    }
+                    else
+                    {
+                        counts[key] += num;
+                    }
+                }            
+            }
+
+
+            //Проверяем, не портится ли список, после его урезания
+            /*int checkSum = 0;
+            foreach (KeyValuePair<string, int> kvp in counts)
+            {
+                checkSum += kvp.Value;
+            }*/
+
+            List<string> toRemove = new List<string>();
+            int otherAmount = 0;
+
+            counts.Add("ПРОЧИЕ", 0);
+            foreach (KeyValuePair<string, int> kvp in counts)
+            {
+                if (kvp.Value < 400)
+                {
+                    otherAmount += kvp.Value;
+                    toRemove.Add(kvp.Key);
+                }
+            }
+
+            foreach(string i in toRemove)
+            {
+                counts.Remove(i);
+            }
+            counts["ПРОЧИЕ"] = otherAmount;
+
+            string res = "";
+
+            /*res += "Контрольная сумма: ";
+            res += "\n";
+            res += checkSum;
+            res += "\n";*/
+
+            int fullAmount = 0;
+            foreach(KeyValuePair<string, int> kvp in counts.OrderByDescending(kvp => kvp.Value))
+            {
+                res += kvp.Key;
+                res += ":    ";
+                res += kvp.Value;
+                res += "\n";
+
+                fullAmount += kvp.Value;
+            }
+            res += "\n\nОбщая сумма:  ";
+            res += fullAmount;
+
+            resultText.Text = res;
+        }
+
+        private void Window_MouseLeftButtonDown_1(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void saveResultBotton_Click(object sender, RoutedEventArgs e)
+        {
+            if (resultText.Text.Trim() == "") return;
+            InputBox ii = new InputBox("Как назовем файл?");
+            string fileName = ii.ShowDialog();
+            if (fileName.Trim() == "") return;
+            if(!Directory.Exists(Environment.CurrentDirectory + "\\reports\\"))
+            {
+                Directory.CreateDirectory(Environment.CurrentDirectory + "\\reports\\");
+            }
+            string FileName = Environment.CurrentDirectory + "\\reports\\" + fileName;
+
+
+
+            StreamWriter s = new StreamWriter(FileName);
+            s.Write(resultText.Text);
+            s.Close();
+        }
+    }
+}
